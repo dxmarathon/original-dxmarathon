b3d60a110324927bc1f5f37108bc71a065b7f8da
Sebastian Delmont <sd@notso.net>
2022-11-07 12:21:24 -0500

    Updates as of Nov 7th

--- dxmarathon.com/submissionlist.php
+++ dxmarathon.com/submissionlist.php
@@ -44,7 +44,7 @@
 		 please contact the DX Marathon manager at k9el at dxmarathon dot com.  If you submitted your log via email, it may take up to 24 hours before your callsign appears.  </h6>
 	<!-- <h5><a href="#logsReceived">Received</a> | <a href="#logsSWL">SWL/Checklog</a> | <a href="#logsDefective">Defective</a> | <a href="#logsDQ">Disqualified</a></h5> -->
  	<h3 id="logsReceived">Received Logs</h3>
- 	<p><br /><br />9H/TA1HZ<br />IU2NUB<br />K4NNK<br />K4VBM<br />NF7D<br />PY4EP<br />W4DTA<br />W7DO<br />YV5AJI<br /></p>
+ 	<p><br /><br />9H/TA1HZ<br />EA4HKF<br />IU2NUB<br />K4NNK<br />K4VBM<br />NF7D<br />PY4EP<br />W4DTA<br />W7DO<br />YV5AJI<br /></p>
  	<!-- <p>&nbsp;</p>
  	<h3 id="logsSWL">SWL, Late and Check Logs</h3>
  	<p></p>
